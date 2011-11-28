fe213e3459b46db94b7d27321e4d595c4e4218cf
Chris <chris.s.mcginn@live.com>
2011-11-28 07:10:30 -0500

    Working model, service work now.

--- Marketing.UI/Client/UserCode/CreateNewCraigsListResponse.cs
+++ Marketing.UI/Client/UserCode/CreateNewCraigsListResponse.cs
@@ -8,25 +8,21 @@ using Microsoft.LightSwitch.Framework.Client;
 using Microsoft.LightSwitch.Presentation;
 using Microsoft.LightSwitch.Presentation.Extensions;
 
-namespace LightSwitchApplication
-{
-    public partial class CreateNewCraigsListResponse
-    {
-        partial void CreateNewCraigsListResponse_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
-        {
-            // Write your code here.
-            this.CraigsListResponseProperty = new CraigsListResponse();
-            this.CraigsListResponseProperty.CraigslistPost = this.DataWorkspace.MarketingDomainServiceData.CraigslistPosts.Where( x => x.Id == this.SelectedCraigslistPostId ).Single();
-            
-        }
-
-        partial void CreateNewCraigsListResponse_Saved()
-        {
-            // Write your code here.
-            this.Close(false);
-            Application.Current.ShowDefaultScreen(this.CraigsListResponseProperty);
-        }
-
+namespace LightSwitchApplication {
+  public partial class CreateNewCraigsListResponse {
+    partial void CreateNewCraigsListResponse_InitializeDataWorkspace( global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo ) {
+      // Write your code here.
+      this.CraigsListResponseProperty = new CraigsListResponse();
+      this.CraigsListResponseProperty.CraigslistPost = this.DataWorkspace.MarketingDomainServiceData.CraigslistPosts.Where( x => x.Id == this.SelectedCraigslistPostId ).Single();
 
     }
+
+    partial void CreateNewCraigsListResponse_Saved() {
+      // Write your code here.
+      this.Close( true );
+
+    }
+
+
+  }
 }
