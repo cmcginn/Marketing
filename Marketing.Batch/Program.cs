a39bc728817ceeb7c2cfd2a122c0ad35b246db04
Chris <chris.s.mcginn@live.com>
2012-02-15 07:27:16 -0500

    modified batch to take users parameter

--- Marketing.Batch/Program.cs
+++ Marketing.Batch/Program.cs
@@ -11,23 +11,39 @@ namespace Marketing.Batch
 {
     class Program
     {
+        static string[] _Args;
         static void Main(string[] args)
         {
+            _Args = args;
             PostsRefresh();
+
         }
         static void PostsRefresh()
         {
-            List<Guid> userList;
-            using (MarketingEntities ctx = new MarketingEntities())
+            var users = _Args.Where(n => n.Contains("users=")).FirstOrDefault();
+            var userNameList = new List<string>();
+            if (users!=null)
             {
-                userList = ctx.aspnet_Membership.Select(n => n.UserId).ToList();
+                var arguments = users.Replace("users=", "").Split(' ');
+                userNameList = arguments.Select(n => n).ToList();
 
             }
+            List<Guid> userList = new List<Guid>();
+            using (MarketingEntities ctx = new MarketingEntities())
+            {
+                ctx.vw_aspnet_MembershipUsers.ToList().ForEach(n =>
+                    {
+                        if (userNameList.Contains(n.UserName))
+                            userList.Add(n.UserId);
+                    });
+
+            }
+            
             var purge = new WorkflowInvoker(new PurgePostsActivity());
             purge.Invoke();
             userList.ForEach(n =>
                 {
-
+                    
                     var inputs = new Dictionary<string, object>();
                     var userId = n;
                     inputs.Add("UserId", userId);
